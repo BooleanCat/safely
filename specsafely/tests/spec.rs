f118b95012567c33f960799b8fa284c4e053290b
BooleanCat <tomgodkin@pm.me>
2021-01-30 10:39:54 +0000

    Fail to act on containers that don't exist

--- specsafely/tests/spec.rs
+++ specsafely/tests/spec.rs
@@ -1,17 +1,66 @@
-use std::path::Path;
+use std::path::PathBuf;
 use std::process::Command;
+
+const SAFELY_PATH: &'static str = "../target/release/safely";
 
 #[test]
 fn it_displays_help() {
-    let output = Command::new(
-        Path::new("..")
-            .join("target")
-            .join("release")
-            .join("safely"),
-    )
-    .arg("--help")
-    .output()
-    .expect("failed to execute process");
+    let output = Command::new(PathBuf::from(SAFELY_PATH))
+        .arg("--help")
+        .output()
+        .expect("failed to execute process");
 
     assert!(String::from_utf8(output.stdout).unwrap().contains("USAGE:"));
+}
+
+#[test]
+fn it_fails_getting_state_for_container_that_does_not_exist() {
+    let output = Command::new(PathBuf::from(SAFELY_PATH))
+        .args(&["state", "foo"])
+        .output()
+        .expect("failed to execute command");
+
+    assert!(!output.status.success());
+    assert!(String::from_utf8(output.stderr)
+        .unwrap()
+        .contains("no such container"));
+}
+
+#[test]
+fn it_fails_killing_a_container_that_does_not_exist() {
+    let output = Command::new(PathBuf::from(SAFELY_PATH))
+        .args(&["kill", "foo"])
+        .output()
+        .expect("failed to execute command");
+
+    assert!(!output.status.success());
+    assert!(String::from_utf8(output.stderr)
+        .unwrap()
+        .contains("no such container"));
+}
+
+#[test]
+fn it_fails_deleting_a_container_that_does_not_exist() {
+    let output = Command::new(PathBuf::from(SAFELY_PATH))
+        .args(&["delete", "foo"])
+        .output()
+        .expect("failed to execute command");
+
+    assert!(!output.status.success());
+    assert!(String::from_utf8(output.stderr)
+        .unwrap()
+        .contains("no such container"));
+}
+
+#[test]
+fn it_fails_starting_a_container_that_does_not_exist() {
+    let output = Command::new(PathBuf::from(SAFELY_PATH))
+        .args(&["start", "foo"])
+        .output()
+        .expect("failed to execute command");
+
+    assert!(!output.status.success());
+    assert!(String::from_utf8(output.stderr)
+        .unwrap()
+        .contains("no such container"));
 }
